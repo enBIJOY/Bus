ec93020375f3d1d2bfd457b7610aea49b34e95dd
BIJOY <BIJOY@DESKTOP-RBD5QOO>
2024-08-20 10:20:38 +0600

    First Commit

---
+++ src/main/java/com/bus/controller/BUS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bus.controller;
+
+/**
+ *
+ * @author BIJOY
+ */
+public class BUS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
